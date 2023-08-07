d6dab68affe794c312016815229ad898dc5fa4ab
Yumax-panda <yuma0wall@gmail.com>
2023-08-08 00:05:42 +0900

    removed SECRET value in order not to raise NO_SECRET error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -13,22 +13,10 @@ const handler = NextAuth({
     }),
   ],
   adapter: PrismaAdapter(prisma),
-  secret: process.env.SECRET,
   session: {
-    strategy: "database",
-    maxAge: 30 * 24 * 60 * 60, // 30 days
-    updateAge: 24 * 60 * 60, // 24 hours
+    strategy: "jwt",
   },
   useSecureCookies: process.env.NODE_ENV === "production",
-  callbacks: {
-    async redirect({ baseUrl }) {
-      return baseUrl;
-    },
-    async session({ session, user }) {
-      if (session?.user) session.user.id = user.id;
-      return session;
-    },
-  },
 });
 
 export { handler as GET, handler as POST };
