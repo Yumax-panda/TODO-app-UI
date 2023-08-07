55157b6efddf01a00324f0edd5afb024dc132dc4
Yumax-panda <yuma0wall@gmail.com>
2023-08-08 01:41:01 +0900

    added 'main' tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={inter.className}>
         <NextAuthProvider>
           <Header />
-          {children}
+          <main>{children}</main>
         </NextAuthProvider>
       </body>
     </html>
