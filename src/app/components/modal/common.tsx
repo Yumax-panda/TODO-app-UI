62e84d12709a40ed008ce8c76ec4f46da830a054
Yumax-panda <yuma0wall@gmail.com>
2023-08-10 02:56:40 +0900

    fixed y-axis

--- src/app/components/modal/common.tsx
+++ src/app/components/modal/common.tsx
@@ -21,7 +21,7 @@ export const ModalWrapper: React.FC<ModalProps> = ({ onClose, children }) => {
   return (
     <div className='z-3 fixed top-0 left-0 h-full w-full bg-gray-300/50' onClick={onClose}>
       <div
-        className='md:w-4/7 absolute inset-0 my-auto mx-auto bg-white px-10 pt-10 pb-4 shadow-lg max-w-lg max-h-96 rounded-md'
+        className='md:w-4/7 absolute inset-0 my-auto mx-auto bg-white px-10 pt-10 pb-4 shadow-lg max-w-lg max-h-fit rounded-md'
         onClick={(e) => e.stopPropagation()}
       >
         {children}
