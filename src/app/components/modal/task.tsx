24c9d1fe1753f1d8ffc23c0125db46713ec63580
Yumax-panda <yuma0wall@gmail.com>
2023-08-09 14:38:29 +0900

    implemented alert when existing missing values

--- src/app/components/modal/task.tsx
+++ src/app/components/modal/task.tsx
@@ -1,6 +1,7 @@
 import { faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 import { NewTaskRequest, Task } from "../../../types/task";
@@ -28,6 +29,7 @@ export const CreateModal: React.FC<CreateModalProps> = ({ userId, onClose }) => 
     deadline: "",
     priority: 0,
   });
+  const router = useRouter();
 
   async function createTask({ request }: { request: NewTaskRequest }) {
     const actual: any = {
@@ -36,6 +38,11 @@ export const CreateModal: React.FC<CreateModalProps> = ({ userId, onClose }) => 
       description: request.description,
       priority: request.priority,
     };
+
+    if (!request.title) {
+      alert("タイトルを入力してください");
+      return null;
+    }
     if (request.deadline) {
       actual.deadline = request.deadline;
     }
@@ -108,10 +115,10 @@ export const CreateModal: React.FC<CreateModalProps> = ({ userId, onClose }) => 
           <button
             type='button'
             className='py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800 sm:p-4'
-            onClick={(e) => {
+            onClick={async (e) => {
               e.preventDefault();
-              createTask({ request });
-              onClose();
+              const newTask = await createTask({ request });
+              if (newTask) router.refresh();
             }}
           >
             追加
