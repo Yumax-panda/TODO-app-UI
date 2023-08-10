050768b36d43783db17d28764dd7ee6612f0c22b
Yumax-panda <yuma0wall@gmail.com>
2023-08-11 02:18:23 +0900

    implemented 'sortBy' query

--- src/app/api/task/route.ts
+++ src/app/api/task/route.ts
@@ -2,9 +2,13 @@ import { NextResponse } from "next/server";
 
 import { prisma } from "../../lib/prisma";
 
+type SortBy = "deadline" | "priority" | "createdAt" | "updatedAt";
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const id = searchParams.get("userId");
+  const _sortBy = searchParams.get("sortBy") as SortBy | null;
+  const sortBy = _sortBy || "deadline";
 
   if (!id) {
     const data = await prisma.task.findMany();
@@ -13,6 +17,7 @@ export async function GET(request: Request) {
 
   const data = await prisma.task.findMany({
     where: { userId: id },
+    orderBy: { [sortBy]: "asc" },
   });
   return NextResponse.json(data);
 }
