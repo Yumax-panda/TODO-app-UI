b75bd99425a3093f0a963c8777ff6c94b784e8a6
Yumax-panda <yuma0wall@gmail.com>
2023-08-13 02:07:30 +0900

    fix rourting

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -74,7 +74,7 @@ const Header = () => {
         >
           <div className='flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:pl-7'>
             <Link
-              href='/content/account'
+              href='/content/profile'
               className='font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500'
             >
               Profile
